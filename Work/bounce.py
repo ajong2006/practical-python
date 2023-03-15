4056fdccc2826b9aad6e5155e301802a5a60e1b4
Ajong <ekokobe.ajong@gmail.com>
2023-03-15 00:27:18 -0500

    Completed 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
-# bounce.py
-#
-# Exercise 1.5
+"""
+bounce.py
+Exercise 1.5
+"""
+
+BOUNCE = 0
+HEIGHT = 100
+
+while BOUNCE <=10:
+    print(BOUNCE, round(HEIGHT, 4))
+    HEIGHT = HEIGHT * (3/5)
+    BOUNCE = BOUNCE +1
+    
