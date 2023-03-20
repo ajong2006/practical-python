19622506c58eb7320d380a7a082e0d30508e83d0
Ajong <ekokobe.ajong@gmail.com>
2023-03-19 22:46:42 -0500

    Completed 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+f = open('./Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+total_cost = 0
+
+for line in f:
+    row = line.split(',')
+    total_cost = total_cost + int(row[1]) * float(row[2])
+    
+print('Total Cost: ', total_cost)
