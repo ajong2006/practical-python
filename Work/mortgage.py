4f1a4fae358c926893d7779dcd20938b53dae81a
Ajong <ekokobe.ajong@gmail.com>
2023-03-19 21:30:28 -0500

    Completed 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,6 +1,6 @@
 """ 
 mortgage.py
-Exercise 1.8
+Exercise 1.9
 """
 
 principal = 500000.0
@@ -9,10 +9,14 @@ payment = 2684.11
 total_paid = 0.0
 num_months = 0
 
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
 while principal > 0:
-    if num_months < 12:
-        principal = principal * (1+rate/12) - (payment + 1000)
-        total_paid = total_paid + payment + 1000
+    if num_months > extra_payment_start_month and num_months < extra_payment_end_month:
+        principal = principal * (1+rate/12) - (payment + extra_payment)
+        total_paid = total_paid + payment + extra_payment
         num_months = num_months + 1
     else:
         principal = principal * (1+rate/12) - payment
