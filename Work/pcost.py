236da15a619e14e8f58505be6f466c2b2727cf3a
Ajong <ekokobe.ajong@gmail.com>
2023-04-01 18:38:03 -0500

    Completed Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,23 @@
 # pcost.py
 #
-# Exercise 1.31
+# Exercise 1.32
+import csv
 
 def portfolio_cost(filename):
 
-    file = open(filename,'rt', encoding="utf8")
-    headers = next(file).split(',')
+    file = open(filename, encoding="utf8")
+    rows = csv.reader(file)
+    headers = next(rows)
+    print('Headers: ', headers)
+
     total_cost = 0
 
-    for line in file:
+    for row in rows:
         try:
-            row = line.split(',')
             total_cost = total_cost + int(row[1]) * float(row[2])
         except ValueError:
-            print("Couldn't parse", line)
-                    
+            print("Couldn't parse", row)
+           
     return total_cost
 
 cost = portfolio_cost('./Data/missing.csv')
